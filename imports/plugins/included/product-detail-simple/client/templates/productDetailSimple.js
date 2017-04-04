b110fdbc39490323f7c54543c9cb6f7ec7b759e3
Chinedu Ofor <chineduofor@Andelas-MacBook-Pro-38.local>
2017-04-04 13:42:03 +0100

    chore(disqus): install disqus [Start #142658553]

--- imports/plugins/included/product-detail-simple/client/templates/productDetailSimple.js
+++ imports/plugins/included/product-detail-simple/client/templates/productDetailSimple.js
@@ -1,5 +1,6 @@
 import { isRevisionControlEnabled } from "/imports/plugins/core/revisions/lib/api";
 import { ProductDetailContainer, PublishContainer } from "../containers";
+import { Template } from "meteor/templating";
 
 Template.productDetailSimple.helpers({
   isEnabled() {
@@ -15,5 +16,15 @@ Template.productDetailSimpleToolbar.helpers({
     return {
       component: PublishContainer
     };
+  }
+});
+
+Template.disquss.helpers({
+  getDisqus() {
+    const d = document;
+    const s = d.createElement("script");
+    s.src = "//http-localhost-3000-13.disqus.com/embed.js";
+    s.setAttribute("data-timestamp", +new Date());
+    (d.head || d.body).appendChild(s);
   }
 });
