045497c5bb0cc7562266c54e55cfe9e91a69063e
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2017-04-24 10:15:12 +0100

    feature(paystack): paystack setup and initialization [142658551]

--- server/security/policy.js
+++ server/security/policy.js
@@ -61,3 +61,5 @@ BrowserPolicy.content.allowOriginForAll("fonts.gstatic.com");
 
 BrowserPolicy.content.allowOriginForAll("enginex.kadira.io");
 BrowserPolicy.content.allowOriginForAll("*.stripe.com");
+BrowserPolicy.content.allowOriginForAll("http://js.paystack.co");
+BrowserPolicy.content.allowOriginForAll("https://paystack.com");
