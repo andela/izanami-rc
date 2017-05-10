9f71164384b6918ade00374502dc68c2af7584e7
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2017-05-10 11:49:50 +0100

    add reaction-cli to package.json

--- imports/plugins/core/layout/client/templates/layout/wallet/wallet.js
+++ imports/plugins/core/layout/client/templates/layout/wallet/wallet.js
@@ -89,7 +89,7 @@ function handlePayment(result) {
 // Paystack payment
 const payWithPaystack = (email, amount) => {
   const paystackConfig = getPaystackSettings();
-  console.log('Paystack Config', paystackConfig)
+  console.log('Paystack Config', paystackConfig);
   const handler = PaystackPop.setup({
     key: paystackConfig.public,
     email: email,
